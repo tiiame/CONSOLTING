0aeafc2c8e0fb4449dbd77a623cc170163110580
Suhrob <atavullayevsuhrob@gmail.com.com>
2024-02-09 16:40:42 +0500

    static_root

--- cor/urls.py
+++ cor/urls.py
@@ -24,5 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static( settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static( settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
